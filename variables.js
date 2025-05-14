e8d8f9b056666d88026aa12eeeb2778029a7e3c3
Vaishalijharbade <vjharbade47@gmail.com>
2025-05-14 08:32:25 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,15 @@
+// Variables
+
+//var | let | const
+
+// var - global
+// re-assign
+// kahi bhi access krlo
+
+// let - local
+// re- assign
+// access sirf khud ke block ke andar
+
+// const- local
+// reassign nhi kr skte
+// access sirf khud ke block ke andar
