db34a54d9df64d92f22cd662c338880514131313
Vaishalijharbade <vjharbade47@gmail.com>
2025-05-17 16:32:58 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 3423232
 let accountEmail = "vaish@gmail.com"
 var accountPassword = "23232"
 accountCity = "Jaipur"
+let accountState;
 
 // accountId = 2 //not allowed
 
@@ -9,6 +10,6 @@ accountEmail = "djhdjh@gmail.com"
 accountPassword = "242323"
 accountCity = "Benguluru"
 
-console.table([accountId, accountPassword, accountEmail, accountCity])
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
 /* Prefer not to use var because
 of issue in block scope and functional scope*/
