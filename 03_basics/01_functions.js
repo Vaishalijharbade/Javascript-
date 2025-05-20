0e4a5c53a0650143df0e07ee45edb670ecd3f966
Vaishalijharbade <vjharbade47@gmail.com>
2025-05-20 17:05:23 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// addTwoNumbers (23,344)
+
+function addTwoNumbers(num1, num2){
+    let result = num1 + num2
+    return result
+}
+
+const result = addTwoNumbers(34,232)
+
+console.log("Result:", result)
+
+function loginUserMessage (username){
+    return `${username} just logged in`
+}
+
+console.log (loginUserMessage ("hitesh"))
