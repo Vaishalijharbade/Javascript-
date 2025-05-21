57275c35dd9c196430b2100eacb392efce4540d5
Vaishalijharbade <vjharbade47@gmail.com>
2025-05-21 07:02:23 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,8 +13,34 @@ const result = addTwoNumbers(34,232)
 
 console.log("Result:", result)
 
-function loginUserMessage (username){
-    return `${username} just logged in`
-}
+// function loginUserMessage (username){
+//     return `${username} just logged in`
+// }
 
-console.log (loginUserMessage ("hitesh"))
+// console.log (loginUserMessage ("hitesh"))
+
+// function loginUserMessage (username){
+//     if(username === undefined){
+//         console.log("Please enter a username")
+//             return
+        
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage("vaishlai"))
+
+//professional code
+
+// "" => empty string ko maan liya jaata hai ki woh false value hai, in the same way
+// "undefined" ko bhi maan liya jaata hai ki woh false value hai but in if else statement if we use
+// undefined tab toh woh execute hi nhi hoga, toh jo username hai usko ulta kardo then it will work
+
+function loginUserMessage(username){
+    if (!username){
+        console.log("Please enter a username")
+        return
+    }
+
+}
+console.log(loginUserMessage())
