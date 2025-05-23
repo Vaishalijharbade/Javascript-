7f7b8189a44231b16bc4d7663c6d22b16c3e4b3f
Vaishalijharbade <vjharbade47@gmail.com>
2025-05-23 05:47:47 +0000

    one and switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+// if
+// const isUserLoggedIn =true
+// if(isUserLoggedIn){
+
+// }
+//<,>,<=,>=,==,!=,===(also checks the type),!==
+
+// if (2==="2"){
+//     console.log("executed")
+// }
+// const score= 300
+// if (score>100){
+//     const power = "fly"
+//     console.log(`User power:${power}`);
+// }
+//  console.log(`User power:${power}`);
+
+// const balance = 1000
+// if(balance > 500) console.log("test");//short hand notation
+
+// if (balance < 500){
+//     console.log("less than 500");
+// }
+// else if(balance < 750){
+//     console.log("less than 750");
+// }
+// else if (balance < 900){
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1200")
+// }
+
+const student = true
+const parents = true
+if (student && parents ){
+    console.log ("Enter the venue")
+}
+const child = true
+const mumma = true
+if (child || mumma){
+    console.log ("You won the prize")
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 6
+switch(month){
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+        default:
+            console.log ("any month")
+            break;
+}
